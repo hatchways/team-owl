446421fd355beff8e3740951f9b787e05616b5b3
Leonard Shen <uvstudio2199@gmail.com>
2020-09-16 20:13:04 -0400

    setup test image upload page

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,21 @@
-import React from "react";
-import { MuiThemeProvider } from "@material-ui/core";
-import { BrowserRouter, Route } from "react-router-dom";
+import React from 'react';
+import { MuiThemeProvider } from '@material-ui/core';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import { theme } from "./themes/theme";
-import LandingPage from "./pages/Landing";
+import { theme } from './themes/theme';
+import LandingPage from './pages/Landing';
+import ImgTest from './pages/ImgTest';
 
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
       <BrowserRouter>
-        <Route path="/" component={LandingPage} />
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/imgtest" component={ImgTest} />
+        </Switch>
       </BrowserRouter>
     </MuiThemeProvider>
   );
